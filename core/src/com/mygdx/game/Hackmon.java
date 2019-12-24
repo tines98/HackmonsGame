06af63ec1bf6b0cb5870ba94085fccc8d9bdc177
KrisAesoey <kris.aesoey@gmail.com>
2019-12-24 04:51:12 +0100

    Restructured Hackmon class structure. Added data files for creatures and types

--- core/src/com/mygdx/game/Hackmon.java
+++ core/src/com/mygdx/game/Hackmon.java
@@ -3,20 +3,77 @@ package com.mygdx.game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Hackmon {
 
     private Texture sprite;
-    private int hp,currHP,str,def,speed;
+    private int baseHP, baseSTAM, baseATK, baseDEF, baseSPATK, baseSPDEF, baseSPEED;
+    private int id, lv;
+    private int hp, stam;
+    private int maxHp, maxStam, atk, def, spAtk, spDef, speed;
+    private String name, type1, type2;
 
-    Hackmon(){
-        hp = 100;
-        currHP=90;
-        str = 50;
-        def = 10;
-        speed = 100;
+    public Hackmon(int id, int lv) {
+        this.id = id;
+        this.lv = lv;
+        initialize(id);
         sprite = new Texture(
-"core/assets/exploudback.png"
+                "core/assets/exploudback.png"
         );
+        hp = maxHp;
+    }
+
+    private void initialize(Integer n) {
+        try {
+            File pokemonList = new File("core/assets/data/pokemon.txt");
+            Scanner sc = new Scanner(pokemonList);
+            String id = "[" + n + "]";
+            while (sc.hasNextLine()) {
+                String data = sc.nextLine();
+                if (data.equals(id)) {
+                    String nameLine = sc.nextLine();
+                    String typeLine = sc.nextLine();
+                    String statLine = sc.nextLine();
+
+                    String [] nameList = nameLine.split("=");
+                    this.name = nameList[1];
+
+                    String [] typeList = typeLine.split("=|,");
+                    if (typeList.length == 3) {
+                        this.type2 = typeList[2];
+                    }
+
+                    this.type1 = typeList[1];
+                    String [] statList = statLine.split("=|,");
+                    baseHP = Integer.parseInt(statList[0]);
+                    baseSTAM = Integer.parseInt(statList[1]);
+                    baseATK = Integer.parseInt(statList[2]);
+                    baseDEF = Integer.parseInt(statList[3]);
+                    baseSPATK = Integer.parseInt(statList[4]);
+                    baseSPDEF = Integer.parseInt(statList[5]);
+                    baseSPEED = Integer.parseInt(statList[6]);
+                    updateStats();
+                    break;
+                }
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("ERROR: Initialization failed");
+            e.printStackTrace();
+        }
+    }
+
+    public void updateStats() {
+        maxHp = ((2 * baseHP * lv) / 100) + lv + 10;
+        maxStam = ((baseSTAM + lv) / 10) + lv;
+        atk = ((2 * baseATK * lv) / 100) + 10;
+        def = ((2 * baseDEF * lv) / 100) + 10;
+        spAtk = ((2 * baseSPATK * lv) / 100) + 10;
+        spDef = ((2 * baseSPDEF * lv) / 100) + 10;
+        speed = ((2 * baseSPEED * lv) / 100) + 10;
     }
 
     public void setToFront(){
@@ -30,11 +87,11 @@ public class Hackmon {
     }
 
     public void takeDamage(int dmg){
-        currHP -= dmg;
+        hp -= dmg;
     }
 
     public int getCurrHP() {
-        return currHP;
+        return hp;
     }
 
     public int getDef() {
@@ -50,10 +107,10 @@ public class Hackmon {
     }
 
     public int getStr() {
-        return str;
+        return atk;
     }
 
     public boolean isFainted(){
-        return currHP<=0;
+        return hp<=0;
     }
 }
