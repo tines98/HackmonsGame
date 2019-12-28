8cb16b4297d24e33edf015a5eadcbc1df5a2f070
KrisAesoey <kris.aesoey@gmail.com>
2019-12-28 18:58:43 +0100

    Added switchMon() method

--- core/src/com/mygdx/game/Trainer.java
+++ core/src/com/mygdx/game/Trainer.java
@@ -32,6 +32,12 @@ public class Trainer {
         return hackmons.get(0);
     }
 
+    private void switchMon(int n) {
+        Hackmon temp = hackmons.get(0);
+        hackmons.set(0, hackmons.get(n));
+        hackmons.set(n, temp);
+    }
+
     private Hackmon removeMon(int i) {
         return hackmons.remove(i);
     }
