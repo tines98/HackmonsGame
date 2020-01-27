a24288a6b4edb500112396036f86d02034310d15
Tines Valen <Tines.Valen@student.uib.no>
2020-01-27 13:19:09 +0100

    added pokemon
    made ItemContainer

---
+++ core/src/com/mygdx/game/items/ItemContainer.java
@@ -0,0 +1,15 @@
+package com.mygdx.game.items;
+
+public class ItemContainer {
+    //POTIONS
+    public static Potion potion = new Potion("Potion",20);
+    public static Potion superPotion = new Potion("Super Potion",60);
+    public static Potion hyperPotion = new Potion("Hyper Potion",200);
+    public static Potion maxPotion = new Potion("Max Potion",99999999);
+
+    //ETHERS
+    public static Ether ether = new Ether("Ether",20);
+    public static Ether superEther = new Ether("Super Ether",60);
+    public static Ether hyperEther = new Ether("Hyper Ether",200);
+    public static Ether maxEther = new Ether("Max Ether",99999999);
+}
