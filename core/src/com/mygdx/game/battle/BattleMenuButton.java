3ec3297d648ddacdcde0dd29bd429c83e23d5329
Tines Valen <Tines.Valen@student.uib.no>
2020-01-28 13:54:51 +0100

    readded BattleMenuButton.java

---
+++ core/src/com/mygdx/game/battle/BattleMenuButton.java
@@ -0,0 +1,42 @@
+package com.mygdx.game.battle;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Align;
+import com.mygdx.game.Colors;
+
+public class BattleMenuButton {
+    int x, y, w, h, textX, textY;
+    Texture color;
+    String text = "ERROR";
+
+    public BattleMenuButton(int x, int y, int w, int h){
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        textX = x+(w/2);
+        textY = y+(h/2);
+    }
+
+    public BattleMenuButton(int x, int y, int w, int h, Texture color) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.color = color;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void render(SpriteBatch batch, BitmapFont font, boolean active){
+        batch.draw(Colors.black,x-1,y-1,w+2,h+2);
+        if (active)
+            batch.draw(Colors.darkGray,x,y,w,h);
+        else batch.draw(Colors.gray,x,y,w,h);
+        font.draw(batch,text,textX,textY+6,0,Align.center,false);
+    }
+}
